31e9ff4d20d8eac191d9612f2e5dfa9b283c07d7
Julian Andres Klode <jak@jak-linux.org>
2018-11-14 22:31:18 +0100

    dial: Implement error handling and set keepalive

--- dial.go
+++ dial.go
@@ -4,28 +4,39 @@ import "net"
 import "log"
 import "time"
 
-var slots map[string]chan net.Conn = make(map[string]chan net.Conn)
+type connOrError struct {
+	conn net.Conn
+	err error
+}
 
+var slots = make(map[string]chan connOrError)
+
+// Dial dials a connection asynchronously, opening a new connection
+// in the background once a connection has been taken. The connections
+// use http.KeepAlive.
 func Dial(network, addr string) (net.Conn, error) {
 	if slots[addr] == nil {
-		slots[addr] = make(chan net.Conn)
+		slots[addr] = make(chan connOrError)
 		go func() {
 			for {
 				t := time.Now()
-				//log.Printf("Starting %s dial to %s", network, addr)
 				conn, err := net.Dial(network, addr)
+				if tcpConn := conn.(*net.TCPConn); tcpConn != nil {
+					if err := conn.(*net.TCPConn).SetKeepAlive(true); err != nil {
+						slots[addr] <- connOrError{nil, err}
+					}
+				}
 				elapsed := time.Now().Sub(t)
 				log.Printf("Finished %s dial to %s in %s", network, addr, elapsed)
-				slots[addr] <- conn
-				//log.Printf("Sent %s dial to %s", network, addr)
 				if err != nil {
-					log.Printf("Error: %s", err)
-					break
+					slots[addr] <- connOrError{nil, err}
+				} else {
+					slots[addr] <- connOrError{conn, nil}
 				}
 			}
 		}()
 	}
 
-	return <-slots[addr], nil
-
+	coe := <-slots[addr]
+	return coe.conn, coe.err
 }
